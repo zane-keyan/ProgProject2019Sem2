3566aef7aad8f352f1153faa2cfa1141633e27c6
Tam Lam <imtamtlam@gmail.com>
2019-03-19 19:14:35 +1100

    navbar and footer

--- homys-car/src/App.js
+++ homys-car/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Home from "./pages/home";
 class App extends Component {
   render() {
     return (
--- homys-car/src/pages/Home.jsx
+++ homys-car/src/pages/Home.jsx
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import NavBar from "../components/navbar";
+import Footer from "../components/footer";
 class Home extends Component {
   state = {};
   render() {
-    return <h1>HelloWorld</h1>;
+    return (
+      <React.Fragment>
+        <NavBar />
+        <div className="container" height="500">
+          <h1>HelloWorld</h1>
+          <p>
+            anjflahwjklfhawlksadfafassjnfklahfklanwlfnklawnflnawlfnlawnflknawlnflawn
+          </p>
+          <p />
+          <p />
+          <p />
+          <p />
+        </div>
+        <Footer />
+      </React.Fragment>
+    );
   }
 }
 
