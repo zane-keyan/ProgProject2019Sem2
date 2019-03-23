00a94d17d6a2f52b09a35ef24058479912239208
Tam Lam <imtamtlam@gmail.com>
2019-03-24 10:48:11 +1100

    dark listview

--- homys-car/src/pages/Home.jsx
+++ homys-car/src/pages/Home.jsx
@@ -14,36 +14,36 @@ class Home extends Component {
           subtitle="Simply selection the location you want to pick up and drop off our vehicles and see what is available for rent"
         />
         <div className="row">
-          <div className="col-md-9 nopadding my-col order-md-2 mapContainer" />
+          <div className="col-lg-9 nopadding my-col order-lg-2 mapContainer" />
 
-          <div className="col-md-3 order-md-1 nopadding my-col">
-            <ul class="list-group">
-              <li class="list-group-item">
+          <div className="col-lg-3 order-lg-1 nopadding my-col">
+            <ul class="list-group bg-dark">
+              <li class="list-group-item bg-dark text-white">
                 Car 1 <br />
                 blah blah blah <br />
                 1km
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item bg-dark text-white">
                 Car 2 <br />
                 blah blah blah <br />
                 1km
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item bg-dark text-white">
                 Car 3 <br />
                 blah blah blah <br />
                 1km
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item bg-dark text-white">
                 Car 4 <br />
                 blah blah blah <br />
                 1km
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item bg-dark text-white">
                 Car 5 <br />
                 blah blah blah <br />
                 1km
               </li>
-              <li class="list-group-item">
+              <li class="list-group-item bg-dark text-white">
                 Car 6 <br />
                 blah blah blah <br />
                 1km
