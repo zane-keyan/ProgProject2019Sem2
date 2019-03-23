e5b3ed514eff28d464523d8dc3d27c922c87e806
Tam Lam <imtamtlam@gmail.com>
2019-03-24 10:30:11 +1100

    homepage layout

--- homys-car/src/components/footer.jsx
+++ homys-car/src/components/footer.jsx
@@ -1,18 +1,5 @@
 import React, { Component } from "react";
 import "./footer.css";
-// class Footer extends Component {
-//   render() {
-//     return (
-//       <footer>
-//         <div className="p-3 mb-2 bg-light text-dark text-center">
-//           © 2019 Copyright:
-//           <a href="https://github.com/Abdosalah/ProgProject2019Sem2"> Team 0</a>
-//         </div>
-//       </footer>
-//     );
-//   }
-// }
-// export default Footer;
 
 class Footer extends Component {
   render() {
--- homys-car/src/pages/Home.jsx
+++ homys-car/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import Jumbotron from "../components/jumbotron";
+import "./home.css";
 class Home extends Component {
   state = {};
   render() {
@@ -12,15 +13,43 @@ class Home extends Component {
           title="Rent now"
           subtitle="Simply selection the location you want to pick up and drop off our vehicles and see what is available for rent"
         />
-        <div className="container" height="500">
-          <h1>HelloWorld</h1>
-          <p>
-            anjflahwjklfhawlksadfafassjnfklahfklanwlfnklawnflnawlfnlawnflknawlnflawn
-          </p>
-          <p />
-          <p />
-          <p />
-          <p />
+        <div className="row">
+          <div className="col-md-9 nopadding my-col order-md-2 mapContainer" />
+
+          <div className="col-md-3 order-md-1 nopadding my-col">
+            <ul class="list-group">
+              <li class="list-group-item">
+                Car 1 <br />
+                blah blah blah <br />
+                1km
+              </li>
+              <li class="list-group-item">
+                Car 2 <br />
+                blah blah blah <br />
+                1km
+              </li>
+              <li class="list-group-item">
+                Car 3 <br />
+                blah blah blah <br />
+                1km
+              </li>
+              <li class="list-group-item">
+                Car 4 <br />
+                blah blah blah <br />
+                1km
+              </li>
+              <li class="list-group-item">
+                Car 5 <br />
+                blah blah blah <br />
+                1km
+              </li>
+              <li class="list-group-item">
+                Car 6 <br />
+                blah blah blah <br />
+                1km
+              </li>
+            </ul>
+          </div>
         </div>
         <Footer />
       </React.Fragment>
