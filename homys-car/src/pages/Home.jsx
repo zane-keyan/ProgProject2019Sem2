402dd4e6c5ccb71d54042c1950e653187ae0e0d7
Tam Lam <imtamtlam@gmail.com>
2019-03-24 10:00:17 +1100

    Add jumbotron and stock images

--- homys-car/src/pages/Home.jsx
+++ homys-car/src/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
+import Jumbotron from "../components/jumbotron";
 class Home extends Component {
   state = {};
   render() {
     return (
       <React.Fragment>
         <NavBar />
+        <Jumbotron
+          title="Rent now"
+          subtitle="Simply selection the location you want to pick up and drop off our vehicles and see what is available for rent"
+        />
         <div className="container" height="500">
           <h1>HelloWorld</h1>
           <p>
