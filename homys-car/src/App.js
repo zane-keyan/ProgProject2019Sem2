5550cfa88f89597f9e312af7d556c97e59ddfafd
Tam Lam <imtamtlam@gmail.com>
2019-03-24 12:12:24 +1100

    Fix could not find home.jsx error

--- homys-car/src/App.js
+++ homys-car/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/home.jsx";
 class App extends Component {
   render() {
     return (
