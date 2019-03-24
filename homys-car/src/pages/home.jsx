d7f5868fb45dd5f362968501fb058e7c2688048f
Tam Lam <imtamtlam@gmail.com>
2019-03-24 19:33:01 +1100

    refine list group appearance

--- homys-car/src/pages/home.jsx
+++ homys-car/src/pages/home.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import NavBar from "../components/navbar";
 import Footer from "../components/footer";
 import Jumbotron from "../components/jumbotron";
+import caricon from "../images/car-icon.png";
 import "./home.css";
 class Home extends Component {
   state = {};
@@ -13,42 +14,48 @@ class Home extends Component {
           title="Rent now"
           subtitle="Simply selection the location you want to pick up and drop off our vehicles and see what is available for rent"
         />
-        <div className="row">
-          <div className="col-lg-7 nopadding my-col order-lg-2 mapContainer" />
-          <div className="col-lg-5 order-lg-1 nopadding my-col">
-            <div className="container-fluid shadow-lg bg-dark ">
-              <ul class="list-group  bg-dark list-group-flush">
-                <li class="list-group-item bg-dark text-white">
-                  Car 1 <br />
-                  blah blah blah <br />
-                  1km
-                </li>
-                <li class="list-group-item bg-dark text-white">
-                  Car 2 <br />
-                  blah blah blah <br />
-                  1km
-                </li>
-                <li class="list-group-item bg-dark text-white">
-                  Car 3 <br />
-                  blah blah blah <br />
-                  1km
-                </li>
-                <li class="list-group-item bg-dark text-white">
-                  Car 4 <br />
-                  blah blah blah <br />
-                  1km
-                </li>
-                <li class="list-group-item bg-dark text-white">
-                  Car 5 <br />
-                  blah blah blah <br />
-                  1km
-                </li>
-                <li class="list-group-item bg-dark text-white">
-                  Car 6 <br />
-                  blah blah blah <br />
-                  1km
-                </li>
-              </ul>
+        <div className="container-fluid bg-dark">
+          <div className="row">
+            <div className="col-lg-7 nopadding my-col order-lg-2 mapContainer" />
+            <div className="col-lg-5 order-lg-1 nopadding my-col">
+              <div className="container-fluid shadow-lg bg-dark ">
+                <ul class="list-group  my-list-group bg-dark list-group-flush">
+                  <li class="list-group-item bg-dark text-white">
+                    <img
+                      src={caricon}
+                      alt="..."
+                      className="img-thumbnail float-left rounded"
+                    />
+                    ModelString Year MakeString BodyString
+                    <br />
+                    Rego: ABCD1234
+                    <button
+                      type="button"
+                      className="btn btn-outline-light  float-right"
+                    >
+                      Rent
+                    </button>
+                    <br /> 1km $price
+                  </li>
+                  <li class="list-group-item bg-dark text-white">
+                    <img
+                      src={caricon}
+                      alt="..."
+                      className="img-thumbnail float-left rounded"
+                    />
+                    ModelString Year MakeString BodyString
+                    <br />
+                    Rego: ABCD1234
+                    <button
+                      type="button"
+                      className="btn btn-outline-light  float-right"
+                    >
+                      Rent
+                    </button>
+                    <br /> 1km $price
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
