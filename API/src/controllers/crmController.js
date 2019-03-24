2ffa3dbf559d6d53b8d0651b98069ce79271382e
Abdo <abdosalah3105@gmail.com>
2019-03-24 14:08:08 +1100

    API infrastructure

---
+++ API/src/controllers/crmController.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+import { CarSchema } from '../models/crmModel';
+
+const Car = mongoose.model('Car', CarSchema);
+
+export const addNewCar = (req, res) => {
+  let newCar = new Car(req.body);
+
+  newCar.save((err, car) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(car);
+  });
+};
+
+// export const getContacts = (req, res) => {
+//   Contact.find({}, (err, contact) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json(contact);
+//   });
+// };
+
+// export const getContactWithID = (req, res) => {
+//   Contact.findById(req.params.contactId, (err, contact) => {
+//   if (err) {
+//     res.send(err);
+//   }
+//   res.json(contact);
+//   });
+// };
+
+// export const updateContact = (req, res) => {
+//   Contact.findOneAndUpdate({_id: req.params.contactId}, req.body, {new: true}, (err, contact) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json(contact);
+//   });
+// };
+
+// export const deleteContact = (req, res) => {
+//   Contact.remove({_id: req.params.contactId }, (err) => {
+//     if (err) {
+//       res.send(err);
+//     }
+//     res.json({ message: 'Succ deleted contact'});
+//   });
+// };
---
+++ API/src/models/crmModel.js
@@ -0,0 +1,55 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+export const CarSchema = new Schema({
+
+  rego: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  year: {
+    type: Number,
+    min: 2000,
+    max: 2019,
+    required: true
+  },
+  make: {
+    type: String,
+    required: true
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  transmission: {
+    type: String,
+    enum: ['Manual', 'Auto'],
+    required: true
+  },
+  availability: {
+    type: Boolean,
+    required: true
+  },
+  damaged: {
+    type: Boolean,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  created_date: {
+    type: Date,
+    default: Date.now
+  }
+
+})
