6291b12832e1edace51de2c12a778d318cd64044
Tam Lam <imtamtlam@gmail.com>
2019-04-05 15:36:40 +1100

    default marker at melbourne, popup info

--- homys-car/src/components/mapContainer.jsx
+++ homys-car/src/components/mapContainer.jsx
@@ -1,23 +1,54 @@
 import React, { Component } from "react";
-import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 const mapStyles = {
   width: "100%",
   height: "100%"
 };
 class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false, //Hides or the shows the infoWindow
+    activeMarker: {}, //Shows the active marker upon click
+    selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+  };
   render() {
     return (
       <Map
         google={this.props.google}
-        zoom={14}
+        zoom={13}
         style={mapStyles}
         initialCenter={{
-          lat: 1,
-          lng: 1
+          lat: -37.814,
+          lng: 144.96332
         }}
-      />
+      >
+        <Marker onClick={this.onMarkerClick} name={"Sup"} />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
 }
 
 export default GoogleApiWrapper({
