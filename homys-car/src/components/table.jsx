820d838c4a0ae977ac8b3f446277b15a598aca09
unknown <s3576664@student.rmit.edu.au>
2019-04-12 16:01:05 +1000

    created admin log page

---
+++ homys-car/src/components/table.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import "./table.css";
+export default function Table(props){
+
+    return(
+        <div className="table">
+        <table>
+            <thead>
+                <tr>
+                        <th> RentalId
+                        </th>
+                        <th>UserId
+                        </th>
+                        <th> Car_rego
+                        </th>
+                        <th> BookingDate
+                        </th>
+                        <th>ReturnDate
+                        </th> 
+                        <th>ReturnLocation
+                        </th> 
+                        <th>TotalPrice
+                        </th>    
+                    </tr>
+                </thead>
+                <tbody>
+                    { props.data.map(row=>
+                        <tr>
+                                <td>{row.rentalid}
+                                    </td>
+                                    <td>{row.userid}
+                                    </td>
+                                    <td>{row.car_rego}
+                                    </td>
+                                    <td>{row.bookingdate}
+                                    </td>
+                                    <td>{row.returndate}
+                                    </td>
+                                    <td>{row.returnLocation}
+                                    </td>
+                                    <td>{row.TotalPrice}
+                                        </td>
+                            </tr>
+                        )
+                    }
+                    </tbody>
+            </table>
+            </div>
+    )
+
+}
