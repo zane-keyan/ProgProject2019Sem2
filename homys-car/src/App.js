ad37b25154ea9ee2fda78c07fa6d9cd9f836f2ad
Tam Lam <imtamtlam@gmail.com>
2019-04-17 18:54:55 +1000

    checkout pages refinement

--- homys-car/src/App.js
+++ homys-car/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/home.jsx";
 import Admin from "./pages/admin";
 import About from "./pages/about";
 import Signin from "./pages/signin";
-import Rent from "./pages/rent";
+import Checkout from "./pages/checkout";
 class App extends Component {
   render() {
     return (
@@ -16,7 +15,7 @@ class App extends Component {
           <Route exact path="/about" component={About} />
           <Route exact path="/signin" component={Signin} />
           <Route exact path="/admin" component={Admin} />
-          <Route exact path="/rent" component={Rent} />
+          <Route exact path="/checkout" component={Checkout} />
         </div>
       </Router>
     );
--- homys-car/src/components/simplePageTitle.jsx
+++ homys-car/src/components/simplePageTitle.jsx
@@ -3,8 +3,10 @@ class SimplePageTitle extends Component {
   render() {
     return (
       <div className="title-container text-center text-light">
-        <h1 className="text-light display-3 checkout-label"> Checkout </h1>
-        <p className="display-4 ">Pay a deposit and be on your way!</p>
+        <h1 className="text-light display-3 checkout-label">
+          {this.props.title}
+        </h1>
+        <p className="display-4 ">{this.props.subtitle}</p>
       </div>
     );
   }
