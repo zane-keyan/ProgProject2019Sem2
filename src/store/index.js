12a4e8d7b632052ee975013e0c2791c27a060918
Zane keyan <zane.keyan@gmail.com>
2019-04-22 16:49:04 +1000

    fixed redux browser issue

--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,12 @@ import rootReducer from './reducers';
 
 const initialState = {};
 
-const middleware = [thunk];
+const middleware = applyMiddleware( thunk );
 
-const store = createStore( rootReducer, initialState, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-));
+const composedEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )|| compose;
+
+const store = createStore( rootReducer, initialState, 
+    composedEnhancers(middleware)
+);
 
 export default store;
