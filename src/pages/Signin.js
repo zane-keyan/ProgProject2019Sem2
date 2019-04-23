2c909bd9fdefde37240239c2a5932f17994a6925
Tam Lam <imtamtlam@gmail.com>
2019-04-23 16:00:29 +1000

    basic form and css

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -2,12 +2,43 @@ import React, { Component } from "react";
 import NavBar from "../components/Navbar";
 import Jumbotron from "../components/Jumbotron";
 import Footer from "../components/Footer";
+import SimplePageTitle from "../components/SimplePageTitle";
 class Signin extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
-        <Jumbotron title="Signin" subtitle="This is Signin page" />
+        <SimplePageTitle title="Sign In" subtitle="" />
+        <div className="container text-light">
+          <div className="form-container">
+            <div class="form-group">
+              <input
+                type="email"
+                class="form-control form-input text-light form-control-lg"
+                id="emailInput"
+                aria-describedby="emailHelp"
+                placeholder="Enter your email"
+              />
+            </div>
+            <div class="form-group">
+              <input
+                type="password"
+                class="form-control form-input text-light form-control-lg"
+                id="passwordInput"
+                placeholder="Enter your password"
+              />
+            </div>
+            <div className="container text-center">
+              <button
+                type="submit"
+                class="btn btn-outline-light btn-lg auth-btn"
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+
         <Footer />
       </React.Fragment>
     );
