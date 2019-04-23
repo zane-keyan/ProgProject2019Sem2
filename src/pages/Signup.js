7b0a8122f7610b0147ff110f530c3ee15bce59da
Tam Lam <imtamtlam@gmail.com>
2019-04-23 19:21:46 +1000

    add submit handle to sign up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -7,6 +7,18 @@ import RightArrowBtn from "../components/RightArrowBtn";
 
 class Signup extends Component {
   state = {};
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("on submit");
+    let form = event.target;
+    console.log("Email VALUE: " + form.elements.email.value);
+    console.log("Password VALUE: " + form.elements.password.value);
+    console.log("firstName VALUE: " + form.elements.firstName.value);
+    console.log("lastName VALUE: " + form.elements.lastName.value);
+    console.log("dateOfBirth VALUE: " + form.elements.dateOfBirth.value);
+    console.log("licenseNo VALUE: " + form.elements.licenseNo.value);
+    console.log("paymentDetail VALUE: " + form.elements.paymentDetail.value);
+  };
   render() {
     return (
       <React.Fragment>
@@ -16,65 +28,70 @@ class Signup extends Component {
           subtitle="Sign up"
           doShowIcon={true}
         />
-        <div className="container text-light">
-          <div className="signup-form-container">
-            <div className="row">
-              <div className="col-md-5 signup-auth-form">
-                <FormGroup
-                  inputType="email"
-                  label="Email"
-                  name="email"
-                  placeholder="Enter your email"
-                />
-                <FormGroup
-                  inputType="password"
-                  label="Password"
-                  name="password"
-                  placeholder="Enter your password"
-                />
-              </div>
-              <div className="col-md-7 signup-detail-form">
-                <div className="row">
-                  <div className="col-6">
-                    <FormGroup
-                      inputType="text"
-                      label="First Name"
-                      name="firstName"
-                      placeholder="First Name"
-                    />
-                  </div>
-                  <div className="col-6">
-                    <FormGroup
-                      inputType="text"
-                      label="Last Name"
-                      name="lastName"
-                      placeholder="Last Name"
-                    />
-                  </div>
+        <form onSubmit={this.handleSubmit}>
+          <div className="container text-light">
+            <div className="signup-form-container">
+              <div className="row">
+                <div className="col-md-5 signup-auth-form">
+                  <FormGroup
+                    inputType="email"
+                    label="Email"
+                    name="email"
+                    placeholder="Enter your email"
+                  />
+                  <FormGroup
+                    inputType="password"
+                    label="Password"
+                    name="password"
+                    placeholder="Enter your password"
+                  />
                 </div>
-                <FormGroup
-                  inputType="date"
-                  label="Date of Birth"
-                  name="dateOfBirth"
-                  placeholder="dd/mm/yyyy"
-                />
-                <FormGroup
-                  inputType="number"
-                  label="License Number"
-                  name="licenseNo"
-                  placeholder="Enter your License number"
-                />
-                <FormGroup
-                  inputType="text"
-                  label="Payment Detail"
-                  name="paymentDetail"
-                  placeholder="Enter your payment detail"
-                />
+                <div className="col-md-7 signup-detail-form">
+                  <div className="row">
+                    <div className="col-6">
+                      <FormGroup
+                        inputType="text"
+                        label="First Name"
+                        name="firstName"
+                        placeholder="First Name"
+                      />
+                    </div>
+                    <div className="col-6">
+                      <FormGroup
+                        inputType="text"
+                        label="Last Name"
+                        name="lastName"
+                        placeholder="Last Name"
+                      />
+                    </div>
+                  </div>
+                  <FormGroup
+                    inputType="date"
+                    label="Date of Birth"
+                    name="dateOfBirth"
+                    placeholder="dd/mm/yyyy"
+                  />
+                  <FormGroup
+                    inputType="number"
+                    label="License Number"
+                    name="licenseNo"
+                    placeholder="Enter your License number"
+                  />
+                  {/* Delete this form group if getting payment detail is not
+                  necessary */}
+                  <FormGroup
+                    inputType="text"
+                    label="Payment Detail"
+                    name="paymentDetail"
+                    placeholder="Enter your payment detail"
+                  />
+                  {/* //////////////// */}
+                </div>
               </div>
+              <RightArrowBtn />
             </div>
-            <RightArrowBtn />
           </div>
-        </div>
+        </form>
         <Footer />
       </React.Fragment>
     );
