85acc0bb9a7389a428cd2861920ffb1def3e9a94
Tam Lam <imtamtlam@gmail.com>
2019-04-23 21:39:41 +1000

    fix broken marker detail view

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { fetchCarsWithDist } from '../store/actions/carActions'
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { fetchCarsWithDist } from "../store/actions/carActions";
 import { Map, GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
 import CurrentLocation from "./Map";
-
 
 class MapContainer extends Component {
   state = {
@@ -18,18 +17,23 @@ class MapContainer extends Component {
     this.props.fetchCarsWithDist();
   }
 
+  onShowDetail = (car, distance) => {
+    car = JSON.stringify(car);
+    distance = JSON.stringify(distance);
+    var distance = JSON.parse(distance);
+    var car = JSON.parse(car);
+    this.props.onShowDetail(car, distance.text);
+  };
   render() {
-
     const markers = this.props.cars.map(item => (
       <Marker
         position={{
           lat: item.car.lat,
           lng: item.car.lng
         }}
-        onClick={() => this.props.onShowDetail(item.car, item.distance)}
+        onClick={() => this.onShowDetail(item.car, item.distance)}
       />
-    ))
-
+    ));
 
     return (
       <CurrentLocation centerAroundCurrentLocation google={this.props.google}>
@@ -69,12 +73,17 @@ class MapContainer extends Component {
 MapContainer.propTypes = {
   fetchCarsWithDist: PropTypes.func.isRequired,
   cars: PropTypes.array.isRequired
-}
+};
 
 const mapStateToProps = state => ({
   cars: state.cars.items
-})
+});
 
-export default connect(mapStateToProps, { fetchCarsWithDist })(GoogleApiWrapper({
-  apiKey: "AIzaSyDEFtWHf9PNwDPk74kYTMLpYzDg8WB7n7Y"
-})(MapContainer));
+export default connect(
+  mapStateToProps,
+  { fetchCarsWithDist }
+)(
+  GoogleApiWrapper({
+    apiKey: "AIzaSyDEFtWHf9PNwDPk74kYTMLpYzDg8WB7n7Y"
+  })(MapContainer)
+);
