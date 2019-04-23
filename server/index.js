cf4dfdc3fb1bbe7cdaeb21944e7d449d0a48ea22
Zane keyan <zane.keyan@gmail.com>
2019-04-23 14:01:38 +1000

    fix file path error

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.get('/', (req, res) =>
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
-  app.use(express.static('client/build'));
+  app.use(express.static('./build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
