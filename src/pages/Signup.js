298bece669df0accda55e7ab032220b7e2cae8c8
Tam Lam <imtamtlam@gmail.com>
2019-04-23 20:39:18 +1000

    form validation for sign up

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,20 +4,45 @@ import Footer from "../components/Footer";
 import SimplePageTitle from "../components/SimplePageTitle";
 import FormGroup from "../components/FormGroup";
 import RightArrowBtn from "../components/RightArrowBtn";
+import Alert from "../components/Alert";
 
 class Signup extends Component {
-  state = {};
+  state = {
+    doesErrorExist: false,
+    errorMessage: "",
+    user: {
+      password: "",
+      email: "",
+      firstName: "",
+      lastName: "",
+      dateOfBirth: "",
+      licenseNo: "",
+      paymentDetail: ""
+    }
+  };
   handleSubmit = event => {
     event.preventDefault();
-    console.log("on submit");
     let form = event.target;
-    console.log("Email VALUE: " + form.elements.email.value);
-    console.log("Password VALUE: " + form.elements.password.value);
-    console.log("firstName VALUE: " + form.elements.firstName.value);
-    console.log("lastName VALUE: " + form.elements.lastName.value);
-    console.log("dateOfBirth VALUE: " + form.elements.dateOfBirth.value);
-    console.log("licenseNo VALUE: " + form.elements.licenseNo.value);
-    console.log("paymentDetail VALUE: " + form.elements.paymentDetail.value);
+    this.setState({
+      user: {
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+        firstName: form.elements.firstName.value,
+        lastName: form.elements.lastName.value,
+        dateOfBirth: form.elements.dateOfBirth.value,
+        licenseNo: form.elements.licenseNo.value,
+        paymentDetail: form.elements.paymentDetail.value
+      }
+    });
+    this.checkForError();
+  };
+  checkForError = () => {
+    Alert.isThereEmptyField(this.state.user)
+      ? this.setState({
+          doesErrorExist: true,
+          errorMessage: Alert.emptyFieldMessage
+        })
+      : this.setState({ doesErrorExist: false });
   };
   render() {
     return (
@@ -32,6 +57,9 @@ class Signup extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="container text-light">
             <div className="signup-form-container">
+              {this.state.doesErrorExist ? (
+                <Alert errorMessage={this.state.errorMessage} />
+              ) : null}
               <div className="row">
                 <div className="col-md-5 signup-auth-form">
                   <FormGroup
