0e4f79c11edd36ae3645c47734534f6900d44562
Tam Lam <imtamtlam@gmail.com>
2019-04-27 17:32:44 +1000

    add alert icon, from margin

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import warningIcon from "../images/warning.png";
 class Alert extends Component {
   render() {
     return (
-      <div class="alert alert-light text-dark" role="alert">
-        {this.props.errorMessage}
+      <div className="alert alert-light text-dark shadow-lg" role="alert">
+        <div className="row">
+          <div className="col-1 text-center">
+            <img className="warning-icon " src={warningIcon} />
+          </div>
+          <div className="col-10">{this.props.errorMessage}</div>
+        </div>
       </div>
     );
   }
