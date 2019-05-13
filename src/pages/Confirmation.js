dc4fbb2e3b7b0759a6930530e741278d3f08bc2c
Tam Lam <imtamtlam@gmail.com>
2019-05-13 16:20:54 +1000

    fix map not recentering on user location

---
+++ src/pages/Confirmation.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Navbar from "../components/Navbar";
+class Confirmation extends Component {
+  state = {};
+  render() {
+    return (
+      <React.Fragment>
+        <Navbar />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Confirmation;
