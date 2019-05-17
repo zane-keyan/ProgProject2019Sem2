28299124d3cab3a194b1fe7edeb20f1435a529b2
Tam Lam <imtamtlam@gmail.com>
2019-05-17 21:04:45 +1000

    ui for rentals

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
-import NavBar from "./Navbar";
-import Footer from "./Footer";
 import { connect } from "react-redux";
 import { fetchRental } from "../store/actions/rentalActions";
-
+import { getDateString, getTimeString } from "../util/dateHelper";
 class Rental extends Component {
   componentDidMount() {
     if (this.props.currentUser) {
       this.props.onFetchRental(this.props.currentUser._id);
     }
   }
-
+  state = {
+    item: {
+      booking_date: "2019-05-17T03:32:54.605Z",
+      _id: "5cde2c6a3fca7218f03ac6fc",
+      car_rego: "KSD481",
+      user_id: "5cce800717e9c712972dab05",
+      on_rent: true,
+      created_at: "2019-05-17T03:37:14.298Z",
+      updated_at: "2019-05-17T03:37:14.298Z"
+    }
+  };
+  click = () => {
+    console.log("Click");
+  };
   render() {
     if (this.props.rentals !== null) {
-      alert("confirmations is defined");
       var rentalItems = this.props.rentals.map(item => (
         <div>
           <button
@@ -33,11 +43,48 @@ class Rental extends Component {
 
     return (
       <React.Fragment>
-        <NavBar />
-        <p>List of Rentals</p>
-        {rentalItems}
+        <div className="tab-content-container">
+          <h1>List of Rentals</h1>
+          {rentalItems}
+          <button
+            className=" btn rental-item-container bg-dark text-light text-left w-100"
+            onCLick={this.click}
+            tabindex="0"
+          >
+            <h4> Car KSD481</h4>
+            {/* <h4>Booking Time</h4> */}
+            Booking Date: {getDateString("2019-05-17T03:37:14.298Z")}{" "}
+            &nbsp;&nbsp;&nbsp;&nbsp;Time:{" "}
+            {getTimeString("2019-05-17T03:37:14.298Z")}
+            {/* <button className="btn btn-primary"> Click</button> */}
+          </button>
 
-        <Footer />
+          <button
+            className=" btn rental-item-container bg-dark text-light text-left w-100"
+            onCLick={this.click()}
+            tabindex="0"
+          >
+            <h4> Car KSD481</h4>
+            {/* <h4>Booking Time</h4> */}
+            Booking Date: {getDateString("2019-05-17T03:37:14.298Z")}{" "}
+            &nbsp;&nbsp;&nbsp;&nbsp;Time:{" "}
+            {getTimeString("2019-05-17T03:37:14.298Z")}
+            {/* <button className="btn btn-primary"> Click</button> */}
+          </button>
+
+          <button
+            className=" btn rental-item-container bg-dark text-light text-left w-100"
+            onCLick={this.click()}
+            tabindex="0"
+          >
+            <h4> Car KSD481</h4>
+            {/* <h4>Booking Time</h4> */}
+            Booking Date: {getDateString("2019-05-17T03:37:14.298Z")}{" "}
+            &nbsp;&nbsp;&nbsp;&nbsp;Time:{" "}
+            {getTimeString("2019-05-17T03:37:14.298Z")}
+            {/* <button className="btn btn-primary"> Click</button> */}
+          </button>
+        </div>
       </React.Fragment>
     );
   }
