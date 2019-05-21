a846202470e1c6e7b45ff419fd90f2a1f229cef8
Tam Lam <imtamtlam@gmail.com>
2019-05-21 13:48:06 +1000

    Simple paypal page, remove checkout car after clicking buy

--- src/pages/Paypal.js
+++ src/pages/Paypal.js
@@ -1,62 +1,83 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
-import queryString from 'query-string';
-import { savePaymentInfo } from '../store/actions/paymentActions'
-import { addConfirmation } from '../store/actions/confirmationActions';
-
+import queryString from "query-string";
+import { savePaymentInfo } from "../store/actions/paymentActions";
+import { addConfirmation } from "../store/actions/confirmationActions";
+import { deleteCheckoutCar } from "../store/actions/carActions";
+import SimplePageTitle from "../components/SimplePageTitle";
 
 class Paypal extends Component {
-
+  state = {
+    rego: ""
+  };
   componentWillMount() {
+    if (this.props.currentCar) {
+      this.setState({ rego: this.props.currentCar.rego });
+    }
     let url = this.props.location.search;
     let params = queryString.parse(url);
-    
-    const user_id = "MY USER ID";
-    const paymentId = params['paymentId']; 
-    const payerId = params['PayerID'];
 
-    if ( paymentId && payerId && user_id )
+    const user_id = "MY USER ID";
+    const paymentId = params["paymentId"];
+    const payerId = params["PayerID"];
+
+    if (paymentId && payerId && user_id)
       this.props.onSavePaymentInfo(user_id, paymentId, payerId);
-    else 
-      console.log("no payment INFO provided")
+    else console.log("no payment INFO provided");
   }
 
   render() {
     let url = this.props.location.search;
     let params = queryString.parse(url);
-    
-    
+
     return (
       <div>
-        <h1>Buy a Hat</h1>
         {/* onSubmit={() => this.props.onAddConfirmation({rego: this.props.currentCar , user_id: this.props.currentUser.user._id})} */}
-        <form action="http://localhost:3001/success" method="get" onSubmit={() => this.props.onAddConfirmation({rego: this.props.currentCar , user_id: this.props.currentUser.user._id})} >
-          <input type="hidden" name="PayerID" value={params['PayerID']}></input>
-          <input type="hidden" name="paymentId" value={params['paymentId']}></input>
-          <input type="submit" value="Buy" ></input>
+        <form
+          action="http://localhost:3001/success"
+          method="get"
+          onSubmit={() => {
+            this.props.onAddConfirmation({
+              rego: this.state.rego,
+              user_id: this.props.currentUser.user._id
+            });
+            this.props.onDeleteCheckoutCar();
+          }}
+          className="text-center"
+        >
+          <input type="hidden" name="PayerID" value={params["PayerID"]} />
+          <input type="hidden" name="paymentId" value={params["paymentId"]} />
+          <SimplePageTitle title="Confirmation" subtitle="Last step" />
+          <input className="btn btn-success btn-lg" type="submit" value="Buy" />
         </form>
       </div>
-    )
+    );
   }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
   return {
-    currentCar: state.cars.checkoutCar.rego,
+    // currentCar: state.cars.checkoutCar.rego,
+    currentCar: state.cars.checkoutCar,
     currentUser: state.auth
-  
-  }
+  };
 }
 
 const mapDispatchToProps = dispatch => {
   return {
+    onDeleteCheckoutCar: () => {
+      dispatch(deleteCheckoutCar());
+    },
     onAddConfirmation: confirmation => {
       dispatch(addConfirmation(confirmation));
     },
-    onSavePaymentInfo: (user_id, paymentId, payerId )=> {
-      dispatch(savePaymentInfo(user_id, paymentId, payerId ));
+    onSavePaymentInfo: (user_id, paymentId, payerId) => {
+      dispatch(savePaymentInfo(user_id, paymentId, payerId));
     }
   };
 };
 
-export default connect(mapStateToProps , mapDispatchToProps)(Paypal);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Paypal);
