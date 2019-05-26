0e908846c3095428e13568e1b3412430db5927ef
Tam Lam <imtamtlam@gmail.com>
2019-05-26 12:42:13 +1000

    return pop up ui

--- src/components/ReturnModal.js
+++ src/components/ReturnModal.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Modal from "react-bootstrap/Modal";
 import { Link } from "react-router-dom";
+import { notifyReturnSucceed } from "./ToastContent";
+import UserDetail from "../components/UserDetail";
 
 class ReturnModal extends Component {
   state = {};
@@ -13,16 +15,45 @@ class ReturnModal extends Component {
           centered
         >
           <Modal.Header className="modal-header" closeButton>
-            <Modal.Title className="modal-title">Details</Modal.Title>
+            <Modal.Title className="modal-title">Return</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <div className="container ">{""}</div>
+            <div className="container text-center">
+              <div>
+                <h1 className="display-3 font-weight-bold">Total</h1>
+
+                <h1 className="display-3 font-weight-bold">$123</h1>
+                <p>Rent by Test</p>
+              </div>
+
+              <div className="text-left">
+                <div className="row">
+                  <div className="col-lg-6">
+                    <UserDetail title="Total Time" content="123h" />
+                  </div>
+                  <div className="col-lg-6">
+                    <UserDetail title="Pricing" content="12$/h" />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-lg-6">
+                    <UserDetail title="Booking date" content="1214" />
+                  </div>
+                  <div className="col-lg-6">
+                    <UserDetail title="Booking Time" content="1214" />
+                  </div>
+                </div>
+              </div>
+            </div>
           </Modal.Body>
           <Modal.Footer className="bg-success modal-footer shadow-lg">
             <Link
               className="btn btn-block rent-btn bg-success text-light shadow-lg"
               to={{
                 pathname: "/"
+              }}
+              onClick={() => {
+                notifyReturnSucceed();
               }}
             >
               Return
