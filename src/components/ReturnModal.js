0053119ba809e29f82ed735c65cbd66f903c93da
Zane keyan <zane.keyan@gmail.com>
2019-05-28 17:30:41 +1000

    added google places api and address input for return modal

--- src/components/ReturnModal.js
+++ src/components/ReturnModal.js
@@ -3,11 +3,67 @@ import Modal from "react-bootstrap/Modal";
 import { Link } from "react-router-dom";
 import { notifyReturnSucceed } from "./ToastContent";
 import UserDetail from "../components/UserDetail";
+import Script from 'react-load-script';
+import SearchBar from 'material-ui-search-bar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
 class ReturnModal extends Component {
-  state = {};
+  // Declare State
+  constructor(props) {
+    super(props);
+
+    // Declare State
+    this.state = {
+      city: '',
+      query: ''
+    };
+
+    // Bind Functions
+    this.handleScriptLoad = this.handleScriptLoad.bind(this);
+    this.handlePlaceSelect = this.handlePlaceSelect.bind(this);
+
+  }
+
+  handleScriptLoad() { 
+    // Declare Options For Autocomplete 
+    alert('script loaded')
+    console.log('laoding script laod')
+    var options = { types: ['(cities)'] }; 
+    
+    // Initialize Google Autocomplete 
+    /*global google*/
+    this.autocomplete = new google.maps.places.Autocomplete(
+                          document.getElementById('autocomplete'),
+                          options ); 
+    // Fire Event when a suggested name is selected
+    this.autocomplete.addListener('place_changed',
+                                  this.handlePlaceSelect); 
+  }
+
+  handlePlaceSelect() {
+
+    
+    console.log('laoding')
+    // Extract City From Address Object
+    let addressObject = this.autocomplete.getPlace();
+    let address = addressObject.address_components;
+
+    alert(address)
+    // Check if address is valid
+    if (address) {
+      // Set State
+      this.setState(
+        {
+          city: address[0].long_name,
+          query: addressObject.formatted_address,
+        }
+      );
+    }
+  }
   render() {
     return (
+        <Script url="https://maps.googleapis.com/maps/api/js?key=AIzaSyCVT0ufJbPLrh4hbunIDrF3TYDAolrNOlg&libraries=places" onLoad={this.handleScriptLoad}  />,
+        <MuiThemeProvider>
       <React.Fragment>
         <Modal
           {...this.props}
@@ -46,7 +102,20 @@ class ReturnModal extends Component {
                 </div>
               </div>
             </div>
+            <div className="modal-search">
+        <Script
+          url="https://maps.googleapis.com/maps/api/js?key=AIzaSyDEFtWHf9PNwDPk74kYTMLpYzDg8WB7n7Y&libraries=places"
+          onLoad={this.handleScriptLoad}
+        />
+        <SearchBar id="autocomplete" placeholder="" hintText="Search City" value={this.state.query}
+          style={{
+            margin: '0 auto',
+            maxWidth: 800,
+          }}
+        />
+      </div>
           </Modal.Body>
+
           <Modal.Footer className="bg-success modal-footer shadow-lg">
             <Link
               className="btn btn-block rent-btn bg-success text-light shadow-lg"
@@ -62,6 +131,7 @@ class ReturnModal extends Component {
           </Modal.Footer>
         </Modal>
       </React.Fragment>
+      </MuiThemeProvider>
     );
   }
 }
