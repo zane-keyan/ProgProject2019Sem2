26f12995813cb629fbd6e5fa837d35df5aea9d1e
BAIGEy <48582458+BAIGEy@users.noreply.github.com>
2019-06-08 21:33:01 +1000

    update_admin_page

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,26 +1,22 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
-import AdminMenu from "../components/AdminMenu";
+import TabM from "../components/tabM";
+class Admin extends Component {
 
-class Admin extends Component{
 
-   
-      render() {
-     
+    render() {
+
         return (
-
-<React.Fragment>
-      <NavBar />
-     <AdminMenu />
-
- <Footer />
-
-</React.Fragment>
-
-    );
-  }      
+            <React.Fragment>
+                <NavBar/>
+                <TabM/>
+                <Footer/>
+            </React.Fragment>
+        );
     }
-    
-  
+}
+
+
 export default Admin;
+
