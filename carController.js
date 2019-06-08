3ea7d72d4e961f1c67a5041e7b408d63401dfb1c
BAIGEy <48582458+BAIGEy@users.noreply.github.com>
2019-06-08 21:20:49 +1000

    add_car/user_controller

---
+++ carController.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Car = require('../models/carModel');
+
+
+const addNewCar = (req, res) => {
+  let newCar = new Car(req.body);
+
+  newCar.save((err, car) => {
+    if (err) {
+      res.send(err);
+    }
+    res.json(car);
+  });
+};
+
+const updateCar = (req, res) => {
+    Car.updateOne({_id: req.body.id}, {$set: {...req.body.data}}, (err, car) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(car);
+    });
+};
+
+const delCar = (req, res) => {
+    Car.deleteOne({_id: req.body.id}, (err, car) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(car);
+    });
+};
+
+
+const getCars = (req, res) => {
+    let data = {}
+    if (req.query.mark === '' || req.query.mark ===null) {
+        Car.find({}, (err, car) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(car);
+        });
+    } else {
+        data.make = req.query.mark;
+        Car.findOne(data, (err, car) => {
+            if (err) {
+                res.send(err);
+            }
+            res.json(car ? [car] : []);
+        });
+    }
+};
+
+module.exports = {addNewCar, getCars, updateCar, delCar};
---
+++ userController.js
@@ -0,0 +1,114 @@
+const bcrypt = require('bcrypt');
+const User = require('../models/userModel');
+
+
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+
+const addNewUser = (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ msg: 'Please enter all fields' })
+    }
+
+    User.findOne({ email })
+        .then(user => {
+            if (user) 
+                return res.status(400).json({ msg: 'User already exists' })});
+
+    let newUser = new User(req.body);
+
+    newUser.save()
+        .then( user => {
+            jwt.sign(
+                { id: user.id },
+                config.get('jwtSecret'),
+                { expiresIn: 3600 },
+                (err , token) => {
+                    if (err) throw err;
+                    res.json({
+                        token,
+                        user: {
+                            id: user.id,
+                            username: user.username,
+                            email: user.email,
+                            isAdmin: user.isAdmin
+                        }
+                    })
+                }
+            )
+        })
+};
+
+const loginUser = (req, res) => {
+    const { email, password } = req.body;
+
+    // Simple Validation
+    if ( !email || !password ) {
+        return res.status(400).json({ msg: 'Please enter all fields'})
+    }
+
+    // Check for existing user
+    User.findOne({ email })
+        .then( user => {
+            if (!user) return res.status(400).json({ msg: 'User does not exist' });
+
+            // Validate password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials' });
+
+                    jwt.sign(
+                        { id: user.id },
+                        config.get('jwtSecret'),
+                        { expiresIn: 3600 },
+                        (err, token) => {
+                            if (err) throw err;
+                            res.json({
+                                token,
+                                user: {
+                                    id: user.id,
+                                    username: user.username,
+                                    email: user.email,
+                                    isAdmin: user.isAdmin
+                                }
+                            })
+                        }
+                    )
+
+                })
+        })
+};
+
+const updateUser  = (req, res) => {
+    console.log(req.body)
+    User.updateOne({_id: req.body.id}, {$set: {...req.body.data}}, (err, car) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(car);
+    });
+};
+const delUser = (req, res) => {
+    User.remove({_id: req.body.id}, (err, car) => {
+        console.log(car)
+        if (err) {
+            res.send(err);
+        }
+        res.json(car);
+    });
+};
+
+
+const getUsers = (req, res) => {
+    User.find({}, (err, user) => {
+        if (err) {
+            res.send(err);
+        }
+        res.json(user);
+    });
+};
+
+module.exports = { addNewUser , loginUser , getUsers,delUser,updateUser};
